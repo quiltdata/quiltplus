8aa468ebe7968ede38c95c50bc660708833e81b6
Ernest Prabhakar <ernest@quiltdata.io>
2022-12-28 11:30:09 -0800

    pass test_make_dataclass

--- quiltplus/schema.py
+++ quiltplus/schema.py
@@ -1,5 +1,18 @@
 # schemas.py
+from dataclasses import make_dataclass
+
+from fondat.data import make_datacls
+
+K_OPT = "?"
+K_DEF = "="
 
 
-def schema2class(dict):
-    None
+def parse(k, v):
+    is_optional = k[0] == K_OPT
+    has_default = K_DEF in v
+    return (k, v)
+
+
+def schema2class(name, fields):
+    list = [parse(k, v) for k, v in fields.items()]
+    return make_dataclass(name, list)
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,16 +1,15 @@
 import importlib.resources
 
-import fondat
 import pytest
 import yaml
 
 import tests
+from quiltplus.schema import schema2class
 
 TDIR = importlib.resources.files(tests)
 TEST_FILE = TDIR / "examples" / "schemas.yml"
 TEST_YAML = TEST_FILE.read_text()
 TEST_DICT = yaml.safe_load(TEST_YAML)
-
 URI_KEY = "QuiltPlusURI"
 
 
@@ -21,6 +20,20 @@ def test_imports():
     assert URI_KEY in TEST_DICT
 
 
-def test_schema2class():
-    schema = TEST_DICT[URI_KEY]
-    assert schema
+def test_make_dataclass():
+    KEY = "Ktest"
+    dict = {"field": "str"}
+    klass = schema2class(KEY, dict)
+    assert klass
+    inst = klass("string")
+    assert inst
+    assert isinstance(inst, klass)
+
+
+def untest_schema2class():
+    fields = TEST_DICT[URI_KEY]
+    assert fields
+    klass = schema2class(URI_KEY, fields)
+    uri_class = globals()[URI_KEY]
+    assert uri_class
+    assert klass == uri_class
