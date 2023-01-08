4a5cb66a4ade573718542f40d17e2f2d46d9b5bb
Ernest Prabhakar <ernest@quiltdata.io>
2023-01-07 22:15:33 -0800

    restful client

--- quiltplus/client.py
+++ quiltplus/client.py
@@ -3,42 +3,85 @@ from pathlib import Path
 from quilt3 import list_packages
 from yaml import Loader, dump, load, safe_load
 
-from .id import K_IDX, QuiltID
+from .id import QuiltID
 from .package import QuiltPackage
 
 ROOT = Path.home() / "Documents" / "QuiltData"
 RECENTS = "recents.yaml"
 
+# get() returns recent QuiltID by numerical index
+# get(index) returns Package with that index
+# delete(index) removes QuiltID with that index from recents
+# put(index) updates Package with that index
+# post() create Package (add to recents)
 
-class QuiltClient:
-    def __init__(self, root=ROOT):
-        self.root = root
-        self.path = root / RECENTS
+
+class QidCache:
+    def __init__(self, path):
+        self.path = path
         self.path.touch(exist_ok=True)
-        print(self.path)
-        print(self.path.is_file())
+        self.load_qids()
 
+    def save_qids(self):
+        recents = [qid.attrs for qid in self.qids]
+        with self.path.open("a+") as f:
+            dump(recents, f)
+
+    def load_qids(self):
         with self.path.open() as f:
-            self.recents = load(f, Loader) or {}
+            recents = load(f, Loader) or []
+            print("recents", recents)
+            self.qids = [QuiltID.FromAttrs(attrs) for attrs in recents]
 
-    async def get(self, qid, asType=None):
-        type = asType or qid.type()
+    def find_qid(self, index):
+        result = [q for q in self.qids if q.index == index]
+        return next(iter(result), None)
+
+    def add_qid(self, qid):
+        self.qids.append(qid)
+        self.save_qids()
+
+    async def delete(self, index):
+        print(f"delete[{index}].size", self.size())
+        qid = self.find_qid(index)
+        print(f"delete.find_qid", qid)
+        if qid:
+            print(f"delete.qids", self.qids)
+            self.qids.remove(qid)
+            print("+delete.qids", self.qids)
+            return qid
+
+    def size(self):
+        return len(self.qids)
+
+
+class QuiltClient(QidCache):
+    def __init__(self, root=ROOT):
+        super().__init__(root / RECENTS)
+        self.root = root
+
+    async def post(self, attrs):
+        qid = QuiltID.FromAttrs(attrs)
+        self.add_qid(qid)
+        return qid
+
+    async def put(self, attrs, index):
+        print("put.size", self.size())
+        await self.delete(index)
+        print("put.delete.size", self.size())
+        qid = QuiltID.FromAttrs(attrs, index)
+        print("put.FromAttrs.size", self.size())
+        self.add_qid(qid)
+        print("put.add_qid.size", self.size())
+        return qid
+
+    async def get(self, index=-1):
+        if index < 1:
+            return self.qids
         try:
-            resource = QuiltPackage(qid)  # Should create appropriate resource by type
-            self.update(qid)
-            return resource
+            qid = self.find_qid(index)
+            # resource = QuiltPackage(qid)
+            return qid
         except Exception as err:
             print(err)
             return None
-
-    async def list(self, registry=None):
-        qids = [QuiltID.FromAttrDict(attr) for attr in self.recents.values()]
-        return qids
-
-    def update(self, qid):
-        print(f"update: {qid.id()}")
-        qid.attr[K_IDX] = len(self.recents)
-        self.recents[qid.id()] = qid.attr
-        print(self.path)
-        with self.path.open("a+") as f:
-            dump(self.recents, f)
