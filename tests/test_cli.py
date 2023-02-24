ab0eb39e77e9e7b5ead57c7885c578b05d6514a0
Ernest Prabhakar <ernest@quiltdata.io>
2023-02-24 13:43:39 -0800

    test_hello_runner

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,4 @@
-import asyncclick as click
+from asyncclick.testing import CliRunner
 
 from .conftest import *
 
@@ -6,3 +6,10 @@ from .conftest import *
 async def test_hello():
     await exec_hello(1, "Ernie")
     assert True
+
+
+async def test_hello_runner():
+    runner = CliRunner()
+    result = await runner.invoke(hello, ["--name", "Peter"])
+    assert result.exit_code == 0
+    assert result.output == "Hello, Peter!\n"
