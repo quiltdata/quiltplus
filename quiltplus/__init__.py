c40b53c202fd0c382b6db462da6021a8aeb00a88
Ernest Prabhakar <ernest@quiltdata.io>
2023-02-27 20:43:48 -0800

    update GitIgnore

--- quiltplus/__init__.py
+++ quiltplus/__init__.py
@@ -4,6 +4,7 @@ from quiltplus.cache import *
 from quiltplus.cli import *
 from quiltplus.config import *
 from quiltplus.id import *
+from quiltplus.ignore import *
 from quiltplus.package import *
 from quiltplus.parse import *
 from quiltplus.unparse import *
