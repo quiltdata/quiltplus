bb37bedf4a33c9f247e23dba9d29d6b5939ce186
Ernest Prabhakar <ernest@quiltdata.io>
2023-05-19 13:22:48 -0700

    import .versions .registry

--- quiltplus/__init__.py
+++ quiltplus/__init__.py
@@ -1,18 +1,20 @@
 import yaml
 
-from quiltplus.cache import QuiltIdCache  # NOQA F401
-from quiltplus.cli import cli  # NOQA F401
-from quiltplus.config import QuiltConfig  # NOQA F401
-from quiltplus.id import QuiltID  # NOQA F401
-from quiltplus.ignore import GitIgnore  # NOQA F401
-from quiltplus.package import QuiltPackage  # NOQA F401
-from quiltplus.parse import (K_BKT, K_HSH, K_PKG, K_PRP, K_PTH,  # NOQA F401
+from .cache import QuiltIdCache  # NOQA F401
+from .cli import cli  # NOQA F401
+from .config import QuiltConfig  # NOQA F401
+from .id import QuiltID  # NOQA F401
+from .ignore import GitIgnore  # NOQA F401
+from .package import QuiltPackage  # NOQA F401
+from .parse import (K_BKT, K_HSH, K_PKG, K_PRP, K_PTH,  # NOQA F401
                              K_STR, K_VER, QuiltParse)
-from quiltplus.unparse import QuiltUnparse  # NOQA F401
+from .registry import QuiltRegistry  # NOQA F401
+from .unparse import QuiltUnparse  # NOQA F401
+from .versions import QuiltVersions  # NOQA F401
 
 
 def default_representer(dumper, data):
-    # Alternatively, use repr() instead str():
+    # Alternatively, use repr() instead of str():
     return dumper.represent_scalar("tag:yaml.org,2002:str", str(data))
 
 
