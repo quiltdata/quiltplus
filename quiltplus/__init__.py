1a342df1358dd61c92f2c0229ab8933215047e5c
Ernest Prabhakar <ernest@quiltdata.io>
2023-06-02 16:18:52 -0700

    yaml representer fixed

--- quiltplus/__init__.py
+++ quiltplus/__init__.py
@@ -8,11 +8,3 @@ from .resource import QuiltResource, QuiltResourceURI  # NOQA F401
 from .type import QuiltType  # NOQA F401
 from .uri import QuiltUri  # NOQA F401
 from .versions import QuiltVersions  # NOQA F401
-
-
-def default_representer(dumper, data):
-    # Alternatively, use repr() instead of str():
-    return dumper.represent_scalar("tag:yaml.org,2002:str", str(data))
-
-
-yaml.representer.SafeRepresenter.add_representer(None, default_representer)  # type: ignore
