6fe6209ce1b94e10182f91ada724c1110dfd1d67
Ernest Prabhakar <ernest@quiltdata.io>
2023-06-10 16:35:10 -0700

    QuiltUri docstring for K_PKG_NAME

--- quiltplus/uri.py
+++ quiltplus/uri.py
@@ -6,6 +6,34 @@ from .type import QuiltType
 
 
 class QuiltUri(QuiltType):
+    """
+    Create and manage Quilt Resources from UnURI attrs.
+
+    >>> reg = "s3://quilt-example"
+    >>> pkg = "quilt/data"
+    >>> pkg_full = f"{pkg}:latest"
+    >>> path = "foo/bar"
+    >>> uri = f"{QuiltUri.PREFIX}{reg}#package={pkg_full}&path={path}"
+    >>> attrs = UnUri(uri).attrs
+    >>> quilt = QuiltUri(attrs)
+    >>> quilt.uri == uri
+    True
+    >>> quilt.registry == reg
+    True
+    >>> quilt.package == pkg
+    True
+    >>> quilt.attrs[QuiltUri.K_PKG] == pkg_full
+    True
+    >>> quilt.attrs[QuiltUri.K_PKG_NAME]
+    'quilt/data'
+    >>> quilt.attrs[QuiltUri.K_PKG_PRE]
+    'quilt'
+    >>> quilt.attrs[QuiltUri.K_PKG_SUF]
+    'data'
+    >>> quilt.attrs[QuiltUri.K_PTH] == path
+    True
+    """
+
     @classmethod
     def FromUnUri(cls, un: UnUri) -> "QuiltUri":
         return cls(un.attrs)
@@ -24,8 +52,7 @@ class QuiltUri(QuiltType):
         self.attrs = attrs
         self.uri = attrs.get(UnUri.K_URI)
         self.registry = f"{attrs.get(UnUri.K_PROT)}://{attrs.get(UnUri.K_HOST)}"
-        package = self.parse_package()
-        self.package: str = package if isinstance(package, str) else ""
+        self.package = self.parse_package()
 
     def __repr__(self):
         return f"QuiltUri({self.uri})"
@@ -47,12 +74,21 @@ class QuiltUri(QuiltType):
             return s[0]
         return False
 
-    def parse_package(self) -> str | bool:
-        return (
+    def parse_package(self) -> str:
+        package = (
             self.split_package(QuiltUri.K_HASH)
             or self.split_package(QuiltUri.K_TAG)
             or self.full_package()
         )
+        sep = QuiltUri.SEP[QuiltUri.K_PKG]
+        if not isinstance(package, str) or sep not in package:
+            return ""
+
+        split = package.split(sep)
+        self.attrs[QuiltUri.K_PKG_NAME] = package
+        self.attrs[QuiltUri.K_PKG_PRE] = split[0]
+        self.attrs[QuiltUri.K_PKG_SUF] = split[1]
+        return package
 
     def has_package(self) -> bool:
         return QuiltUri.SEP[QuiltUri.K_PKG] in self.package if self.package else False
