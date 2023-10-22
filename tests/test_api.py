941e0dea55c91567c4eda18c95c5f8d0f856151c
Ernest Prabhakar <ernest@quiltdata.io>
2023-10-22 08:35:48 +0200

    async test

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,17 +3,17 @@ from quiltplus import get_package, get_registry, list_package, list_registry  # 
 from .conftest import TEST_BKT, TEST_PKG
 
 
-def test_api_get_registry():
-    assert get_registry(TEST_BKT) is not None
+async def test_api_get_registry():
+    assert await get_registry(TEST_BKT) is not None
 
 
-def test_api_list_registry():
-    assert list_registry(TEST_BKT) is not None
+async def test_api_list_registry():
+    assert await list_registry(TEST_BKT) is not None
 
 
-def test_api_get_package():
-    assert get_package(TEST_BKT, TEST_PKG) is not None
+async def test_api_get_package():
+    assert await get_package(TEST_BKT, TEST_PKG) is not None
 
 
-def test_api_list_package():
-    assert list_package(TEST_BKT, TEST_PKG) is not None
+async def test_api_list_package():
+    assert await list_package(TEST_BKT, TEST_PKG) is not None
