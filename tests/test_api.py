e044740d93a19c9d5c68dc28db6f2f14b570d687
Ernest Prabhakar <ernest@quiltdata.io>
2023-10-22 08:26:37 +0200

    test_api

---
+++ tests/test_api.py
@@ -0,0 +1,19 @@
+from quiltplus import get_package, get_registry, list_package, list_registry  # NOQA F401
+
+from conftest import TEST_BKT, TEST_PKG
+
+
+def test_api_get_registry():
+    assert get_registry(TEST_BKT) is not None
+
+
+def test_api_list_registry():
+    assert list_registry(TEST_BKT) is not None
+
+
+def test_api_get_package():
+    assert get_package(TEST_BKT, TEST_PKG) is not None
+
+
+def test_api_list_package():
+    assert list_package(TEST_BKT, TEST_PKG) is not None
